b6d827ae0d4f3c8fb7bb14b1daf170825a169a78
satoru <satoru4107@gmail.com>
2025-03-29 23:13:24 +0900

    Updates

---
+++ precache-manifest.ec07e0ec5a63a0a7917c0ef4aa2785ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99e8a19644a682484f3f691f1e911d33",
+    "url": "/negaku-salon-app/index.html"
+  },
+  {
+    "revision": "3b7db5557b558b4f5cba",
+    "url": "/negaku-salon-app/static/css/main.cd41c07e.chunk.css"
+  },
+  {
+    "revision": "f67758d96db1aec950f4",
+    "url": "/negaku-salon-app/static/js/2.701610b1.chunk.js"
+  },
+  {
+    "revision": "80c22b709854abab6800481175e1e427",
+    "url": "/negaku-salon-app/static/js/2.701610b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8751ece060d2c676e7e0",
+    "url": "/negaku-salon-app/static/js/3.b75b75f3.chunk.js"
+  },
+  {
+    "revision": "3b7db5557b558b4f5cba",
+    "url": "/negaku-salon-app/static/js/main.b2edb0d4.chunk.js"
+  },
+  {
+    "revision": "9ebca7f069dd971bbd91",
+    "url": "/negaku-salon-app/static/js/runtime-main.d271ff04.js"
+  }
+]);
