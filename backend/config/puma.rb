58e1604cd196a91df01336239a4c80d464e84e5c
nakamura <asdfas@my.mac.local>
2025-08-19 20:30:13 +0900

    fix

--- backend/config/puma.rb
+++ backend/config/puma.rb
@@ -1,45 +1,37 @@
-# This configuration file will be evaluated by Puma. The top-level methods that
-# are invoked here are part of Puma's configuration DSL. For more information
-# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
 
-# Puma can serve each request in a thread from an internal thread pool.
-# The `threads` method setting takes two numbers: a minimum and maximum.
-# Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to 5 threads for minimum
-# and maximum; this matches the default thread size of Active Record.
-max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
-min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
-threads min_threads_count, max_threads_count
+# Threads
+max_threads = Integer(ENV.fetch("RAILS_MAX_THREADS", 5))
+min_threads = Integer(ENV.fetch("RAILS_MIN_THREADS", max_threads))
+threads min_threads, max_threads
 
-rails_env = ENV.fetch("RAILS_ENV") { "development" }
-port ENV.fetch("PORT", "8080")
-
-if rails_env == "production"
-  # If you are running more than 1 thread per process, the workers count
-  # should be equal to the number of processors (CPU cores) in production.
-  #
-  # It defaults to 1 because it's impossible to reliably detect how many
-  # CPU cores are available. Make sure to set the `WEB_CONCURRENCY` environment
-  # variable to match the number of processors.
-  worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { 1 })
-  if worker_count > 1
-    workers worker_count
-  else
-    preload_app!
-  end
-end
-# Specifies the `worker_timeout` threshold that Puma will use to wait before
-# terminating a worker in development environments.
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
-
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
-
-# Specifies the `environment` that Puma will run in.
+# Environment
+rails_env = ENV.fetch("RAILS_ENV", ENV.fetch("RACK_ENV", "development"))
 environment rails_env
 
-# Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+# Bind/Port
+default_port = rails_env == "production" ? "8080" : "3000"
+bind "tcp://0.0.0.0:#{ENV.fetch('PORT', default_port)}"
 
-# Allow puma to be restarted by `bin/rails restart` command.
+# Workers
+web_concurrency = Integer(ENV.fetch("WEB_CONCURRENCY", 0))
+if web_concurrency > 0
+  workers web_concurrency
+  preload_app!
+end
+
+# Devでの長めのタイムアウト
+worker_timeout 3600 if rails_env == "development"
+
+# PID / tmp_restart
+pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
 plugin :tmp_restart
+
+# マルチプロセス時にDB再接続
+on_worker_boot do
+  begin
+    require "active_record"
+    ActiveRecord::Base.establish_connection if defined?(ActiveRecord::Base)
+  rescue => e
+    warn "ActiveRecord reconnect failed: #{e.class}: #{e.message}"
+  end
+end
